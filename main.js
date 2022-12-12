e43084333e0f84643884dabfe95980c0f3a421e6
Kainoa Ubaldo-Brabo <kaibrabo@gmail.com>
2022-12-11 21:35:55 -0800

    add Camera

--- main.js
+++ main.js
@@ -3,7 +3,20 @@ import * as THREE from 'three';
 
 function main() {
     const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer({canvas});
+    const renderer = new THREE.WebGLRenderer({ canvas });
+
+    // Camera
+    const fov = 75;     // fov: degrees, others: radians
+    const aspect = 2;   // canvas default
+    const near = 0.1;
+    const far = 5;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far); 
+
+    camera.position.z = 2;
+
+    // Scene
+    // const scene = new THREE.scene();
+
 }
 
 main();
