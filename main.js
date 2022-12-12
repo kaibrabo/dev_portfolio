e7fe433e6020c09d34d18c84b80fdf826cd98ca7
Kainoa Ubaldo-Brabo <kaibrabo@gmail.com>
2022-12-11 20:31:18 -0800

    add ThreeJS CDN

---
+++ main.js
@@ -0,0 +1,10 @@
+// THREE-JS
+import * as THREE from 'three';
+
+function main() {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({canvas});
+}
+
+main();
+console.log("Three JS loaded");
